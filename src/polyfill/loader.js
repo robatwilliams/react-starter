c4c2df16ba19810ef7e8f6d770845216db0c7cb7
Robat Williams <robatwilliams@gmail.com>
2017-12-09 11:23:03 +0000

    Remove now needless nesting in polyfills loader

--- src/polyfill/loader.js
+++ src/polyfill/loader.js
@@ -1,17 +1,18 @@
+// ES5 only in this file
 function loader(polyfillsScript) {
   var browserSupportsAll = window.fetch && window.Promise;
 
   if (!browserSupportsAll) {
     appendScript(polyfillsScript);
   }
+}
 
-  function appendScript(src) {
-    var script = document.createElement('script');
-    script.src = src;
+function appendScript(src) {
+  var script = document.createElement('script');
+  script.src = src;
 
-    // append after the currently-executing script tag
-    document.body.appendChild(script);
-  }
+  // append after the currently-executing script tag
+  document.body.appendChild(script);
 }
 
 loader(window.reactStarterPolyfills);
