e58481e02a83d8c32ef11d9477afa88fc77e6292
Robat Williams <robatwilliams@gmail.com>
2017-12-09 11:52:24 +0000

    Fix loading hazard with dynamically-inserted script

--- src/polyfill/loader.js
+++ src/polyfill/loader.js
@@ -11,6 +11,9 @@ function appendScript(src) {
   var script = document.createElement('script');
   script.src = src;
 
+  // Async is the default for dynamically-inserted scripts
+  script.async = false;
+
   // Don't block loading of subsequent scripts.
   // Since this loader is inline, and other defered scripts haven't yet been seen by the parser,
   // the inserted script will go before those in the run-order queue.
